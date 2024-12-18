598ae4ee70844fb1c4e423c58c0213aa81bfe8a3
Jessica Cardoso <145391050+Jessyruby@users.noreply.github.com>
2024-12-18 00:47:56 -0300

    scroll do widow e da section transporte e header fixed

--- script.js
+++ script.js
@@ -7,40 +7,73 @@ function handleScroll() {
         const sectionTop = section.getBoundingClientRect().top;
         const sectionBottom = section.getBoundingClientRect().bottom;
 
-        // Adiciona a classe 'show' se a seção estiver visível
         if (sectionTop < windowHeight && sectionBottom > 0) {
             section.classList.add('show');
         } else {
-            // Remove a classe 'show' se a seção sair completamente da viewport
+           
             section.classList.remove('show');
         }
     });
 }
 
-window.addEventListener('scroll', handleScroll);
+window.addEventListener('scroll', function() {
+    let header = document.querySelector('#cabecalho')
+    header.classList.toggle('rolagem', window.scrollY > 0)
 
-// Chamada inicial para verificar as seções já visíveis ao carregar a página
+    handleScroll()
+});
+
 handleScroll();
 
+//////////////////////////////////////////////////////////////////////////////
 
-function selecao(circleNum) {
+const circles = document.querySelectorAll('.circle')
+const buses = document.querySelectorAll('.bus')
+const container = document.querySelector('.onibus')
 
-    if(circleNum == 1) {
+circles.forEach((circle, index) => {
+  circle.addEventListener('click', () => {
+    const targetBus = buses[index]
+    const containerRect = container.getBoundingClientRect()
+    const targetRect = targetBus.getBoundingClientRect()
+
+    const offset = targetRect.left - containerRect.left + container.scrollLeft
+
+    container.scrollTo({
+      left: offset,
+      behavior: 'smooth',
+    })
+
+    if(index == 0) {
         document.querySelector('#circle1').style.background = '#fc9a30'
     } else {
         document.querySelector('#circle1').style.background = '#D9D9D9'
     }
     
-    if(circleNum == 2) {
+    if(index == 1) {
         document.querySelector('#circle2').style.background = '#fc9a30'
     } else {
         document.querySelector('#circle2').style.background = '#D9D9D9'
     }
 
-    if(circleNum == 3) {
+    if(index == 2) {
         document.querySelector('#circle3').style.background = '#fc9a30'
     } else {
         document.querySelector('#circle3').style.background = '#D9D9D9'
     }
-}
 
+  })
+})
+
+function scrollToSection(id) {
+    const element = document.querySelector(id);
+
+    if (id == '.about' || id == '.servicos' || id == '.banner') {
+        window.scrollTo({ 
+            top: element.offsetTop - 100,
+            behavior: 'smooth' 
+        });
+    } else {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
